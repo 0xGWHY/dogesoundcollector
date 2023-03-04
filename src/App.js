ac98f064282d1fd0a4c06392482d23ef14d0fd00
GH <gwanghyunjeon@gmail.com>
2023-03-04 22:34:52 +0900

    22.03.04

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import Caver from "caver-js";
 import { useInfiniteQuery } from "react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useInView } from "react-intersection-observer";
+import { MakeDogeSound } from "./Fixed";
 
 function App() {
   // const caver = new Caver("https://public-node-api.klaytnapi.com/v1/cypress");
   const caver = new Caver("wss://public-en-cypress.klaytn.net/ws");
-  const [blockNumber, setBlockNumber] = useState(0);
-  caver.rpc.klay.getBlock("latest").then((e) => setBlockNumber(caver.utils.hexToNumber(e.number)));
+  // const caver = new Caver("https://public-node-api.klaytnapi.com/v1/cypress");
+  const [data, setData] = useState("");
+  const [latestBlock, setLatestBlock] = useState();
+  const [hasBlock, setHasBlock] = useState(false);
   const contract = caver.contract.create(
     [
       {
@@ -325,70 +330,251 @@ function App() {
     ],
     "0x1a693c175e510959f37d54acff0fac0dac8d9a2d"
   );
-  const subscribe = contract.subscribe("allEvents", function (error, event) {
-    console.log(event);
-  });
-  const allEvents = contract.events.allEvents((e) => {
-    console.log(e);
-  });
+  const [active, setActive] = useState(false);
+
+  // const subscribe = contract.subscribe("allEvents", function (error, event) {
+  //   console.log(event);
+  // });
+  // const allEvents = contract.events.allEvents((e) => {
+  //   console.log(e);
+  // });
   const pastEvents = contract.getPastEvents("allEvents", { fromBlock: 114558433, toBlock: "latest" }).then((e) => e);
 
-  // const query = useInfiniteQuery(
-  //   ["logs"],
-  //   async ({ pageParam = 1 }) => {
-  //     let result = [];
-  //     let tempBlockNumber = blockNumber;
-  //     while (result.length < 21) {
-  //       console.log(tempBlockNumber);
-  //       let temp = contract.getPastEvents("allEvents", { fromBlock: tempBlockNumber - 1000, toBlock: tempBlockNumber });
-  //       console.log(temp);
-  //       result = [...temp, ...result];
-  //       tempBlockNumber = tempBlockNumber - 1001;
-  //     }
-  //     setBlockNumber(tempBlockNumber);
-  //     return { data: result, nextPage: pageParam + 1 };
-  //   },
-  //   { getNextPageParam: (lastPage) => lastPage.nextPage }
-  // );
+  const toDSC = (number) => {
+    const url = `https://v3.dogesound.club/mates/${number}`;
+    window.open(url, "_blank");
+  };
+  const toScope = (tx) => {
+    const url = `https://scope.klaytn.com/tx/${tx}?tabId=inputData`;
+    window.open(url, "_blank");
+  };
 
-  // let tempBlockNumber = blockNumber;
-  // if (tempBlockNumber !== 0) {
-  //   let result = [];
-  //   while (result.length < 21) {
-  //     let temp = [];
-  //     contract.getPastEvents("allEvents", { fromBlock: tempBlockNumber - 10000, toBlock: tempBlockNumber }).then((e) => {
-  //       console.log(e);
-  //       temp = [...e];
-  //     });
-  //     tempBlockNumber = tempBlockNumber - 10001;
-  //     result = [...temp, ...result];
-  //   }
-  // }
-  let tempBlockNumber = blockNumber;
-  if (tempBlockNumber > 0) {
+  const fetch = async (block) => {
+    let blockCount = block;
     let result = [];
-    while (result.length < 21) {
-      console.log(tempBlockNumber);
-      contract.getPastEvents("allEvents", { fromBlock: tempBlockNumber - 10000, toBlock: tempBlockNumber }).then((e) => {
+    while (result.length < 20 && blockCount > 0) {
+      await contract.getPastEvents("allEvents", { fromBlock: blockCount - 100000, toBlock: blockCount }).then((e) => {
         result = [...e, ...result];
       });
-      tempBlockNumber = tempBlockNumber - 10001;
+      blockCount -= 100000;
     }
-  }
+    return { data: result.reverse(), lastBlock: blockCount };
+  };
+
+  caver.rpc.klay
+    .getBlock("latest")
+    .then((e) => {
+      setLatestBlock(caver.utils.hexToNumber(e.number));
+    })
+    .then(() => setHasBlock(true));
+
+  const dogeSounds = useInfiniteQuery(["dogeSounds"], ({ pageParam = latestBlock }) => fetch(pageParam), {
+    getNextPageParam: (lastPage) => lastPage.lastBlock,
+    refetchOnWindowFocus: false,
+    enabled: hasBlock,
+  });
+
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 1,
+    onChange: (inView) => {
+      if (inView) {
+        // console.log("fetch");
+        dogeSounds.fetchNextPage();
+      }
+    },
+  });
+
+  const skeletonHandler = () => {
+    let temp = [];
+    for (let i = 0; i < 20; i++) {
+      temp.push(
+        <SkeletonUnit key={i}>
+          <div className="box-profile">
+            <div className="img-skeleton skeleton-animation"></div>
+          </div>
+          <div className="box-body">
+            <div className="box-header">
+              <div className="mate-name skeleton-animation"></div>
+            </div>
+            <div className="box-content">
+              <div className="description skeleton-animation"></div>
+            </div>
+          </div>
+        </SkeletonUnit>
+      );
+    }
+    return temp;
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Header>
+        <div>Doge Sound Collector</div>
+        <p>"너의 개소리가 들려"</p>
+      </Header>
+      <MakeDogeSound active={active} setActive={setActive} />
+      {dogeSounds.data?.pages
+        ?.flatMap((flatMap) => flatMap.data)
+        .map((map) => (
+          <MsgUnit key={map.transactionHash}>
+            <div className="box-profile">
+              <img src={`https://storage.googleapis.com/dsc-mate/336/dscMate-${map.returnValues.mateId}.png`} alt={map.returnValues.mateId} onClick={() => toDSC(map.returnValues.mateId)}></img>
+            </div>
+            <div className="box-body">
+              <div className="box-header">
+                <div className="mate-name" onClick={() => toDSC(map.returnValues.mateId)}>{`MATE #${map.returnValues.mateId}${map.returnValues.name ? ` - ${map.returnValues.name}` : ""}`}</div>
+              </div>
+              <div className="box-content">
+                <p>{map.returnValues.message}</p>
+              </div>
+              <div className="check-tx">
+                <p onClick={() => toScope(map.transactionHash)}>블록체인에서 확인하기</p>
+              </div>
+            </div>
+          </MsgUnit>
+        ))}
+      {dogeSounds.isFetching ? skeletonHandler() : ""}
+      {dogeSounds.isFetching ? "" : <div className="cursor" ref={ref}></div>}
     </div>
   );
 }
 
 export default App;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem 1rem 3rem 1rem;
+  width: 100%;
+  max-width: 800px;
+
+  div {
+    font-size: 4rem;
+  }
+`;
+
+const MsgUnit = styled.div`
+  display: flex;
+  margin: 0 1rem 1rem 1rem;
+  background-color: var(--color-box-content);
+  border-radius: 15px;
+  padding: 1rem 1rem 1.5rem 1rem;
+  width: 100%;
+  max-width: 800px;
+  transition: all 0.3s;
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: rgba(255, 255, 0, 0.5) 0px 0px 10px;
+  }
+  .box-profile {
+    margin-right: 1.5rem;
+    img {
+      width: 4rem;
+      height: auto;
+      min-height: 4rem;
+      background-color: var(--color-background);
+      border-radius: 1000px;
+      cursor: pointer;
+    }
+  }
+  .box-header {
+    display: flex;
+    margin-bottom: 0.5rem;
+    div {
+      font-size: 1.3rem;
+      color: yellow;
+    }
+    .mate-name {
+      cursor: pointer;
+    }
+  }
+  .box-body {
+    flex: 1 0 0;
+  }
+  .box-content {
+    background-color: var(--color-box-content);
+    p {
+      line-height: 150%;
+      word-break: break-all;
+      width: 100%;
+      cursor: default;
+    }
+  }
+  .check-tx {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-top: 1rem;
+    p {
+      color: yellow;
+      cursor: pointer;
+    }
+  }
+`;
+
+const SkeletonUnit = styled.div`
+  display: flex;
+  margin: 0 1rem 1rem 1rem;
+  background-color: var(--color-box-content);
+  border-radius: 15px;
+  padding: 1rem;
+  /* width: 100%; */
+  height: 7rem;
+  width: 100%;
+  max-width: 800px;
+
+  .skeleton-animation {
+    @keyframes gradient {
+      0% {
+        background-position: 0% 50%;
+      }
+      50% {
+        background-position: 100% 50%;
+      }
+      100% {
+        background-position: 0% 50%;
+      }
+    }
+    background-image: linear-gradient(-90deg, var(--color-background), var(--color-box-header), var(--color-background));
+    animation: gradient 2s ease infinite;
+    background-size: 200% auto;
+  }
+  .box-profile {
+    margin-right: 1.5rem;
+    div {
+      width: 4rem;
+      height: 4rem;
+      border-radius: 1000px;
+    }
+  }
+  .box-header {
+    display: flex;
+    margin-bottom: 1rem;
+
+    div {
+      font-size: 1.3rem;
+      color: yellow;
+    }
+    .mate-name {
+      width: 7rem;
+      height: 1.5rem;
+      border-radius: 1000px;
+    }
+  }
+  .box-body {
+    flex: 1 0 0;
+  }
+  .box-content {
+    background-color: var(--color-box-content);
+    div {
+      word-break: break-all;
+      width: 100%;
+      height: 1.5rem;
+      border-radius: 1000px;
+    }
+  }
+`;
---
+++ src/Fixed.js
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+import { useEffect, useRef } from "react";
+
+export const MakeDogeSound = ({ active, setActive }) => {
+  const ref = useRef(null);
+  const menuHandler = (e) => {
+    if (active && !ref.current?.contains(e.target)) {
+      setActive(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", (e) => {
+      menuHandler(e);
+    });
+    return window.removeEventListener("click", (e) => {
+      menuHandler(e);
+    });
+  }, [active]);
+
+  return (
+    <Fixed
+      ref={ref}
+      active={active}
+      onClick={() => {
+        if (!active) {
+          setActive(true);
+        }
+      }}
+    >
+      {active ? (
+        <>
+          <div className="subtitle">나도 개소리 싸보기 🐶</div>
+          <div className="description">클레이튼 블록체인에 기록되기 때문에 수정, 삭제가 불가능해. 알고있지?</div>
+          <textarea placeholder="저의 개소리는요.."></textarea>
+          <div className="tx-send">
+            <button
+              onClick={() => {
+                console.log("ok");
+              }}
+            >
+              준비중..
+            </button>
+          </div>
+        </>
+      ) : (
+        "왈왈!!"
+      )}
+    </Fixed>
+  );
+};
+
+const Fixed = styled.div`
+  /* background-color: #5215fc; */
+  position: fixed;
+  bottom: 0;
+  z-index: 90;
+  background-color: var(--color-box-content);
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: ${(props) => (props.active ? "flex-start" : "center")};
+  align-items: center;
+  height: ${(props) => (props.active ? "20rem" : "2.5rem")};
+  color: ${(props) => (props.active ? "white" : "yellow")};
+  font-size: 1.2rem;
+  border-top: 2px solid yellow;
+  padding: 1rem 0 1rem 0;
+  cursor: ${(props) => (props.active ? "default" : "pointer")};
+  transition: height 0.3s;
+  textarea {
+    flex: 1 0 0;
+    max-width: 600px;
+    resize: none;
+    padding: 1rem 1rem 2rem 1rem;
+    background-color: var(--color-box-header);
+    border: none;
+    color: white;
+    width: 100%;
+    height: 5rem;
+    outline: none;
+    border-radius: 15px;
+    line-height: 150%;
+    margin: 0 0 1rem 0;
+  }
+  .subtitle {
+    font-size: 1.5rem;
+    margin: 0 0 1rem 0;
+  }
+  .description {
+    font-size: 0.8rem;
+    margin: 0 0 1rem 0;
+  }
+  .tx-send {
+    width: 100%;
+    max-width: 600px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    button {
+      border: none;
+      height: 1.5rem;
+      width: 5rem;
+      background-color: black;
+      color: yellow;
+      border-radius: 5px;
+      font-weight: 500;
+      cursor: pointer;
+    }
+  }
+`;
